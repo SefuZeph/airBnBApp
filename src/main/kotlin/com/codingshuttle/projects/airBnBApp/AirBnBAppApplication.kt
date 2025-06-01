2be1629e698e94e93b255389823f598842d88f1f
Zephaniah <zephaniahsefuoduori@gmail.com>
2025-06-01 16:25:01 +0300

    Initial commit

---
+++ src/main/kotlin/com/codingshuttle/projects/airBnBApp/AirBnBAppApplication.kt
@@ -0,0 +1,11 @@
+package com.codingshuttle.projects.airBnBApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirBnBAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirBnBAppApplication>(*args)
+}
